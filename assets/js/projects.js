5a81dad15f6c16ad0ede06294524a83f6d338225
NK <oscarfu0721@gmail.com>
2023-04-19 13:55:07 +0100

    init

---
+++ assets/js/projects.js
@@ -0,0 +1,59 @@
+const projects = [
+  {
+    name: "NYX",
+    description: "The first CODM statistics and builds bot for Discord",
+    url: "https://github.com/night0721/cath.exe",
+  },
+  {
+    name: "NYX API",
+    description:
+      "API to serve purposes of NYX and cath npm package using Next.js API Routes",
+    url: "https://api.night0721.me",
+  },
+  {
+    name: "cath",
+    description:
+      "Powerful API Package to scrape data from NYX API, providing useful JS/TS functions and Discord Bot utilities functions such as giveaways",
+    url: "https://github.com/night0721/cath",
+  },
+  {
+    name: "NXC File Host",
+    description:
+      "A Next.js site to act as server of ShareX, allow you to shorten URLs, upload image, paste codes, with a simple and easy to use UI",
+    url: "https://github.com/night0721/nxc-host",
+  },
+  {
+    name: "Auth Bot",
+    description:
+      "Discord Bot to verify user and give them roles, giving server owner the ability to pull back members incase of termination or forgot login",
+    url: "https://github.com/night0721/Auth",
+  },
+  {
+    name: "RPC",
+    description: "Rich Presence template for Discord",
+    url: "https://github.com/night0721/RPC_Night",
+  },
+  {
+    name: "YouTube Downloader",
+    description:
+      "Tool to download YouTube videos in website, CLI and application",
+    url: "https://github.com/night0721/youtube-downloader",
+  },
+  {
+    name: "NullValkyrie",
+    description:
+      "Minecraft 1.19 Plugin for Spigot servers with features in popular servers such as Hypixel",
+    url: "https://github.com/night0721/NullValkyrie",
+  },
+  {
+    name: "NV-Paper",
+    description: "NullValkyrie but in Kotlin for Paper servers",
+    url: "https://github.com/night0721/NV-Paper",
+  },
+  {
+    name: "Nova",
+    description:
+      "Minecraft Plugins for a full skywars experience, with Lobby, Game Server and Network for Spigot and BungeeCord",
+    url: "https://github.com/night0721/Nova-Network",
+  },
+];
